237f11efa340f9bdbe3b5c2cc1a59e169a0e4b57
MD MUSA ALAM <157529747+mdmusahub@users.noreply.github.com>
2025-10-23 12:08:40 +0530

    first commit

---
+++ src/main/java/com/mecaps/ridingBookingSystem/RidingBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.mecaps.ridingBookingSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RidingBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RidingBookingSystemApplication.class, args);
+	}
+
+}
