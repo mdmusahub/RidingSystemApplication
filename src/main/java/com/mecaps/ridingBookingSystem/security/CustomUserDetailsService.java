84041b323875f6ca97f4aa2ef56d70ec6004e8f8
saadansaryy <ansarisaad990@gmail.com>
2025-10-27 14:02:17 +0530

    feat(auth): add jwt authentication

---
+++ src/main/java/com/mecaps/ridingBookingSystem/security/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.mecaps.ridingBookingSystem.security;
+
+import com.mecaps.ridingBookingSystem.entity.User;
+import com.mecaps.ridingBookingSystem.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new RuntimeException("USER NOT FOUND"));
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(String.valueOf(user.getRole()))
+                .build();
+    }
+}
