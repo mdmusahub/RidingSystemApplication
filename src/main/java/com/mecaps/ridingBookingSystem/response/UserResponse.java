3a1ac28cb3e762a270a67bc43a3a93b3bcbc92f0
Myasir18 <mohmmadyasir18@gmail.com>
2025-11-04 13:18:35 +0530

    Add Role field in UserResponse

--- src/main/java/com/mecaps/ridingBookingSystem/response/UserResponse.java
+++ src/main/java/com/mecaps/ridingBookingSystem/response/UserResponse.java
@@ -1,5 +1,6 @@
 package com.mecaps.ridingBookingSystem.response;
 
+import com.mecaps.ridingBookingSystem.entity.Role;
 import com.mecaps.ridingBookingSystem.entity.User;
 import lombok.Data;
 import lombok.Getter;
@@ -12,10 +13,12 @@ public class UserResponse {
     private Long id;
     private String fullName;
     private String email;
+    private Role role;
 
     public UserResponse(User user) {
         this.id = user.getId();
         this.fullName = user.getFullName();
         this.email = user.getEmail();
+        this.role = user.getRole();
     }
 }
