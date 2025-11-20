21299a3163f531d5afa385d9f42f5215b8d302df
gulwaiz123 <mohdgulwaiz44@gmail.com>
2025-11-20 12:26:27 +0530

    checking errors

--- src/main/java/com/mecaps/ridingBookingSystem/config/RazorpayConfig.java
+++ src/main/java/com/mecaps/ridingBookingSystem/config/RazorpayConfig.java
@@ -1,6 +1,7 @@
 package com.mecaps.ridingBookingSystem.config;
 
 import com.razorpay.RazorpayClient;
+import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -8,9 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class RazorpayConfig {
-
-    private static final Logger logger = LoggerFactory.getLogger(RazorpayConfig.class);
 
     @Value("${razorpay.key_id}")
     private String razorpayKeyId;
@@ -21,10 +21,10 @@ public class RazorpayConfig {
     @Bean
     public RazorpayClient razorpayClient() {
         try {
-            logger.info("Initializing Razorpay Client...");
+            log.info("Initializing Razorpay Client...");
             return new RazorpayClient(razorpayKeyId, razorpaySecretKey);
         } catch (Exception e) {
-            logger.error("Failed to initialize Razorpay Client: {}", e.getMessage(), e);
+            log.error("Failed to initialize Razorpay Client: {}", e.getMessage(), e);
             throw new RuntimeException("Unable to initialize Razorpay Client", e);
         }
     }
--- src/main/java/com/mecaps/ridingBookingSystem/entity/Driver.java
+++ src/main/java/com/mecaps/ridingBookingSystem/entity/Driver.java
@@ -48,4 +48,3 @@ public class Driver {
     private Location location;
 
 }
-
